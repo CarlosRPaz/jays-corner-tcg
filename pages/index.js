77ed87e41dbdaa9d4f5b0e5995ead5516ef4738b
CarlosRPaz <trojan.carlos44@gmail.com>
2022-02-26 15:48:43 -0800

    Initial commit to new repo

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 
-import { commerce } from "../lib/commerce";
+import {commerce} from "../lib/commerce";
 
 import Header from "../components/Header";
 import ProductList from "../components/ProductList";
 import ProductGrid from "../components/ProductGrid";
 
 export async function getStaticProps() {
-  const { data } = await commerce.products.list();
+  const {data} = await commerce.products.list();
 
-  const products = data.filter(({ active }) => active);
+  const products = data.filter(({active}) => active);
 
   return {
     props: {
@@ -20,11 +20,12 @@ export async function getStaticProps() {
   };
 }
 
-function IndexPage({ products }) {
+function IndexPage({products}) {
   return (
     <>
       <Head>
-        <title>ChopChop</title>
+        <title>Jay's Corner TCG</title>
+        <link rel="icon" href="/JCTCG_logo.png" />
       </Head>
       <div className="md:min-h-screen md:flex md:items-center">
         <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-10">
@@ -33,7 +34,7 @@ function IndexPage({ products }) {
 
             <motion.div
               className="md:py-12 hidden md:block md:sticky md:top-0"
-              initial={{ opacity: 0, y: 50 }}
+              initial={{opacity: 0, y: 50}}
               animate={{
                 opacity: 1,
                 y: 0,
@@ -41,7 +42,7 @@ function IndexPage({ products }) {
                   delay: 0.25,
                 },
               }}
-              exit={{ opacity: 0, y: -50 }}
+              exit={{opacity: 0, y: -50}}
             >
               <h1 className="font-serif italic text-xl md:text-3xl">Shop:</h1>
 
@@ -53,9 +54,9 @@ function IndexPage({ products }) {
 
           <motion.div
             className="md:min-h-screen py-6 md:py-12 flex items-center md:w-1/2 md:z-40"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -50 }}
+            initial={{opacity: 0, y: 50}}
+            animate={{opacity: 1, y: 0}}
+            exit={{opacity: 0, y: -50}}
           >
             <ProductGrid
               products={products}
